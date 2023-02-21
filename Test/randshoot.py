21c1135b9bc3f0e95959e182452b2150b39fb88e
Danqu555 <42883003+Danqu555@users.noreply.github.com>
2023-02-21 19:16:35 +1100

    Added values to assign to coordinates for random shooting

---
+++ Test/randshoot.py
@@ -0,0 +1,22 @@
+import time
+
+# Defines random number function
+def rand_num(minimum, maximum):
+    now = str(time.perf_counter())
+    rnd = int(now[::-1][:3:])/1000
+    return(round(minimum + rnd*(maximum-minimum)))
+
+# Assigns a range in which numbers can be generated
+# Note: if 1 or 9, it counts as the same value because it's based on rounding
+# For example, as numbers from 1.5 to 2.5 get rounded to 2, meaning that both
+# 1 and 9 will only have a 0.5 range of values that will round to it.
+# Hence, using both the min and max value to correspond to a single value balances it out.
+min = 1
+max = 9
+
+# Generates random coordinates of the shot
+shootX = int(rand_num(min, max))
+shootY = int(rand_num(min, max))
+
+# Temporary, to check if working
+print(shootX, shootY)
